6789c9362856097a515077ec7e75610568d4c1fa
Ahirui Otsu <otsuahirui@gmail.com>
2019-11-18 14:39:17 +0900

    Update

---
+++ chapter4/p4.1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import rbst
+
+class N(rbst.RbstNode):
+    @classmethod
+    def p(cls, n, x):
+        if n is None:
+            return
+        N.p(n.right, x + 4)
+        print(f'{" " * x}{n.value:2}')
+        N.p(n.left, x + 4)
+
+class T(rbst.Rbst):
+    def p(self):
+        N.p(self._root, 0)
+
+def main():
+    a = [
+        3, 5, 7, 10, 15, 16, 18, 19, 20,
+        23, 24, 33, 35, 40, 41, 42, 46,
+    ]
+    t = T()
+    for i, e in enumerate(a):
+        t.insert(i, e)
+    t.p()
+
+if __name__ == '__main__':
+    main()
